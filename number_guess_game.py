496f0d1253e80661346ca790204cc0236577f0e4
Aditya <anupama12sg@gmail.com>
2025-12-01 22:18:49 +0530

    new project: number guessing & added comments

---
+++ number_guess_game.py
@@ -0,0 +1,11 @@
+# Generate a random number (system does this)
+#Loop
+ # Ask the user to guess the number
+ # If the number is not valid
+ #  Print an error
+ # If the number < guess_number
+ #  Print too low
+ # If the number > guess number
+ #  Print too high
+ # Else
+ #  Print Congrats! You got that right!
