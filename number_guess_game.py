50f061a1ff7d1133dad26e8073ef940479da6557
Aditya <anupama12sg@gmail.com>
2025-12-01 22:30:38 +0530

    chore: print the guess no

--- number_guess_game.py
+++ number_guess_game.py
@@ -1,5 +1,11 @@
 # Generate a random number (system does this)
+import random
+
+number_to_guess = random.randint(1, 100)
+
 #Loop
+guess = int(input("Guess the number between 1 to 100: "))
+print(guess)
  # Ask the user to guess the number
  # If the number is not valid
  #  Print an error
