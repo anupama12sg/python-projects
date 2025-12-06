c663a143dc7582ad083d24be7f946ac0d41e83f5
Aditya <anupama12sg@gmail.com>
2025-12-06 18:06:12 +0530

    chore: added comments for the game

---
+++ rock_paper_scissor.py
@@ -0,0 +1,9 @@
+# Ask the user to make a choice
+# If choice is not valid
+#   Print an error message
+# Let the computer make a random choice
+# Print choices (emojis)
+# Detemine the winner
+# Ask the user if they want to continue
+# If not
+#  Exit the game
