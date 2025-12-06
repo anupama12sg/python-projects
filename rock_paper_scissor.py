68715947448105dab0519480fb332b34e565958d
Aditya <anupama12sg@gmail.com>
2025-12-06 19:20:48 +0530

    created a dictionary

--- rock_paper_scissor.py
+++ rock_paper_scissor.py
@@ -2,10 +2,18 @@ import random
 
 choices = ('r', 'p', 's')
 
+# Dictionary
+# key -> value
+# 'r' -> '🪨'
+# 'p' -> '📄'
+# 's' -> '✂️'
+emojis = {'r': '🪨', 'p': '📄', 's': '✂️'}
 user_choice = input("Rock, paper, scissor (r/p/s):").lower()
 if user_choice not in choices:
     print("Invalid Choice.")
 computer_choice = random.choice(choices)
+print(f"Your choice: {user_choice}")
+print(f"Computer's choice: {computer_choice}")
 
 
 # Ask the user to make a choice
